373487a5100fb864a7c9de6ae83dca467ab6a179
michaelchristwin <kelechukwuchristwin@gmail.com>
2023-12-25 17:26:01 +0100

    Init mint method

---
+++ actions/hypercerts.ts
@@ -0,0 +1,53 @@
+import { HypercertClient, HypercertMetadata } from "@hypercerts-org/sdk";
+import { createWalletClient, custom } from "viem";
+import { mainnet } from "viem/chains";
+import { TransferRestrictions, formatHypercertData } from "@hypercerts-org/sdk";
+declare let window: any;
+
+interface MyMetadata {
+  name: string;
+  description: string;
+  external_url?: string | undefined;
+  image: string;
+  version: string;
+  properties?:
+    | {
+        trait_type: string;
+        value: string;
+      }[]
+    | undefined;
+  impactScope: string[];
+  excludedImpactScope: string[];
+  workScope: string[];
+  excludedWorkScope: string[];
+  workTimeframeStart: number;
+  workTimeframeEnd: number;
+  impactTimeframeStart: number;
+  impactTimeframeEnd: number;
+  contributors: string[];
+  rights: string[];
+  excludedRights: string[];
+}
+const nftStorageToken = process.env.NEXT_PUBLIC_NFTSTORAGE;
+
+const walletClient = createWalletClient({
+  chain: mainnet,
+  transport: custom(window.ethereum),
+});
+export const client = new HypercertClient({
+  chainId: BigInt(5), // goerli testnet
+  walletClient,
+  nftStorageToken,
+});
+//@ts-ignore
+
+export async function MintHypercert(props: MyMetadata) {
+  const { data } = formatHypercertData(props);
+  const totalUnits = BigInt(10000);
+
+  const txHash = await client.mintClaim(
+    data as HypercertMetadata,
+    totalUnits,
+    TransferRestrictions.FromCreatorOnly
+  );
+}
