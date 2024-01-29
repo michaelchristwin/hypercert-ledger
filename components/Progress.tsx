5128942fc54473e803bceeb93c2fd2b9da143d05
michaelchristwin <kelechukwuchristwin@gmail.com>
2024-01-29 14:33:51 +0100

    Resized progress

--- components/Progress.tsx
+++ components/Progress.tsx
@@ -3,11 +3,17 @@ import { forwardRef } from "react";
 const ProgressPopup = forwardRef(function ProgressPopup(props, ref) {
   return (
     <dialog
-      className={`lg:w-[45%] md:w-[45%] w-[90%] h-[350px] px-[80px] py-[50px] backdrop:bg-neutral-900/90 inset-0 backdrop-blur z-[30] rounded-xl`}
+      className={`lg:w-[45%] md:w-[45%] w-[90%] h-[350px] lg:px-[80px] md:px-[60px] px-[20px] py-[50px] backdrop:bg-neutral-900/90 inset-0 backdrop-blur z-[30] rounded-xl`}
       ref={ref as React.LegacyRef<HTMLDialogElement> | undefined}
     >
-      <p className={`block text-[25px] font-bold`}>Processing Hypercert</p>
-      <p className={`block text-[18px] italic text-neutral-700`}>
+      <p
+        className={`block lg:text-[25px] md:text-[20px] text-[19px] font-bold`}
+      >
+        Processing Hypercert
+      </p>
+      <p
+        className={`block lg:text-[18px] md:text-[18px] text-[16px] italic text-neutral-700`}
+      >
         Please keep this tab open until completion
       </p>
       <div className="loader">
