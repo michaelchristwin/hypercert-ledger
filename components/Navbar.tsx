52d5027fcf9e5515de1bb33ee7561d1db84298ee
michaelchristwin <kelechukwuchristwin@gmail.com>
2024-02-12 13:22:07 +0100

    Added logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@
 import { useAppContext } from "@/context/appContext";
 import { DM_Sans } from "next/font/google";
 import Link from "next/link";
+import Image from "next/image";
 const font = DM_Sans({
   weight: "600",
   subsets: ["latin"],
@@ -26,6 +27,13 @@ function Navbar() {
       className={`flex w-full h-[90px] lg:px-9 md:px-7 px-2 items-center justify-between z-20 fixed top-0 shadow bg-[#ffffff] backdrop-filter backdrop-blur-[20px] bg-opacity-10`}
     >
       <Link href={`/`} className={`flex w-fit items-center space-x-1`}>
+        <Image
+          src={`/brandlogo.jpg`}
+          alt="Hyperminter logo"
+          width={30}
+          height={30}
+          className={`rounded-full w-[30px] h-[30px]`}
+        />
         <p
           className={`lg:text-[23px] md:text-[22px] text-[19px] font-bold bg-gradient-to-r from-[#3d8b8d] to-[rgba(252,0,255)] text-transparent text-center inline-block bg-clip-text `}
         >
