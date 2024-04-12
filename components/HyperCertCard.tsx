e9ec66fb8ad162a468663426f4b38b981a3d139e
michaelchristwin <kelechukwuchristwin@gmail.com>
2024-04-12 10:38:28 +0100

    Disabled for maintenance

--- components/HyperCertCard.tsx
+++ components/HyperCertCard.tsx
@@ -120,6 +120,7 @@ function HyperCertCard({
         <button
           className={`text-black mx-auto disabled:bg-gray-300 disabled:text-gray-300 disabled:opacity-100 disabled:active:opacity-100 bg-["${gradient}"] backdrop-filter backdrop-brightness-150 block mt-[40px] w-fit hover:opacity-60 hover:border active:opacity-50 px-2 h-[35px] rounded-lg`}
           type="button"
+          disabled
           onClick={handleClick}
         >
           Mint Hypercert
