c195e495740ab90310ccd9d9524afea68fc29c43
michaelchristwin <kelechukwuchristwin@gmail.com>
2025-01-06 17:49:45 +0100

    Chore: Updated footer year

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,10 @@ function Footer() {
       className={`w-full absolute bottom-[0px] h-[40px] text-white bg-neutral-700 flex justify-center items-end`}
     >
       <p className={`font-bold`}>
-        &copy;<a href="https://twitter.com/spark_eco">Spark 2024</a>
+        &copy;
+        <a href="https://twitter.com/spark_eco">
+          Spark {new Date().getFullYear()}
+        </a>
       </p>
     </div>
   );
